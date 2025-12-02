e1f0b10d69ee9043e1f3b6a141c57b39b862095c
ArpãoCeleste <8240558@estg.ipp.pt>
2025-12-02 10:39:41 +0000

    freaky commit 6

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ const customFrontendUrl = process.env.FRONTEND_URL || '';
 
 const allowedOrigins = [
   customFrontendUrl,
-  'https://pwa-all-app.vercel.app',
+  'https://pwa-appnerpa.vercel.app/',
 ].filter(Boolean);
 
 const isAllowedOrigin = (origin) =>
